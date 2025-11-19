a5baf09d595ff202d929711328fd6e4e377983b7
Finlay Williams <finlayw@palantir.com>
2025-11-19 16:27:02 +0000

    no set prop

--- src/main/java/com/palantir/gradle/shadowjar/ShadowJarPlugin.java
+++ src/main/java/com/palantir/gradle/shadowjar/ShadowJarPlugin.java
@@ -33,7 +33,6 @@ import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.artifacts.ExternalModuleDependency;
 import org.gradle.api.artifacts.ResolvedDependency;
 import org.gradle.api.plugins.JavaPlugin;
@@ -210,12 +209,10 @@ public class ShadowJarPlugin implements Plugin<Project> {
                 });
 
         rejectedFromShading.configure(conf -> conf.getDependencies()
-                .addAllLater(project.getObjects()
-                        .setProperty(Dependency.class)
-                        .value(shadowingCalculation.map(calc -> calc.rejectedShadedModules().stream()
+                .addAllLater(shadowingCalculation.map(calc -> calc.rejectedShadedModules().stream()
                                 .map(this::depToString)
                                 .map(project.getDependencies()::create)
-                                .collect(Collectors.toSet())))));
+                                .collect(Collectors.toSet()))));
 
         TaskProvider<ShadowJarConfigurationTask> shadowJarConfigurationTask = project.getTasks()
                 .register("relocateShadowJar", ShadowJarConfigurationTask.class, relocateTask -> {
